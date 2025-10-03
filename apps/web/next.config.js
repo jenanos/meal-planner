8c0820653a26aed41810c8b2c95bba32b772411a
jenanos <jens.osberg@gmail.com>
2025-10-03 10:57:57 +0200

    feat: update environment and Docker configurations for improved setup and compatibility

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
   reactStrictMode: true,
+  output: 'standalone',
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://api:4000/:path*' },
+    ];
+  },
 };
 
 module.exports = nextConfig;
